045ba7c85054f330e055ca24dd43337fd9ad6b99
Fdrv <oldgot@gmail.com>
2021-07-14 14:18:58 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -6,24 +7,28 @@ from more_itertools import chunked
 
 
 def on_reload():
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
-
-    template = env.get_template('template.html')
-
     with open('destfolder/jsonfolder/books.json', 'r',
               encoding='UTF-8') as file:
         books = json.load(file)
-    books_chunked = list(chunked(books, 2))
-    rendered_page = template.render(
-        books=books_chunked,
-    )
+    pages = list(chunked(books, 20))
+    os.makedirs('pages', exist_ok=True)
+    for number, page in enumerate(pages):
+        print(page)
+        env = Environment(
+            loader=FileSystemLoader('.'),
+            autoescape=select_autoescape(['html', 'xml'])
+        )
+        template = env.get_template('template.html')
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
-    print('Site rebuilt')
+        books_chunked = list(chunked(page, 2))
+        rendered_page = template.render(
+            books=books_chunked,
+        )
+
+        with open(f'pages/index{number + 1}.html', 'w',
+                  encoding="utf8") as file:
+            file.write(rendered_page)
+        print(f'Page {number + 1} rebuilt')
 
 
 on_reload()
