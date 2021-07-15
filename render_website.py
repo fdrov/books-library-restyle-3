d88259f8158e1830c142c34d334d991c28bcec24
Fdrv <oldgot@gmail.com>
2021-07-15 21:43:08 +0300

    Fix template.html

--- render_website.py
+++ render_website.py
@@ -7,7 +7,7 @@ from more_itertools import chunked
 
 
 def on_reload():
-    with open('destfolder/jsonfolder/books.json', 'r',
+    with open('static/jsonfolder/books.json', 'r',
               encoding='UTF-8') as file:
         books = json.load(file)
     pages = list(chunked(books, 5))
